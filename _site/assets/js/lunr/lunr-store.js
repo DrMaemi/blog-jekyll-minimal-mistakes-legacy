a76a3940b7dce988874db935e6dfbf09552650ba
DrMaemi <leesh1510@ajou.ac.kr>
2022-02-06 02:12:52 +0900

    First post: How to build python development environment

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Post: Standard",
+        "excerpt":"All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a garden, and she plucked another flower and ran with it to her mother. I suppose she...","categories": ["C++"],
+        "tags": ["First"],
+        "url": "/c++/post-standard/",
+        "teaser": null
+      },{
+        "title": "1. 파이썬 개발 환경 설정",
+        "excerpt":"파이썬 환경 설정 목차 1. 아나콘다 1.1. 설치 1.2. 가상환경 생성 1.3. 패키지 설치 1.4. 설치된 패키지 삭제 1.5. 가상환경 복제 2. 주피터 노트북 - 가상 환경 연동 3. VS Code - 가상환경 연동 1. 아나콘다 1.1. 설치 https://www.anaconda.com/products/individual - 공식 배포 버전 https://repo.anaconda.com/archive/ - 버전 별 다운, 쉘 명령어...","categories": ["Programming-Language","Python"],
+        "tags": ["Anaconda","Jupyter Notebook","VSCode"],
+        "url": "/programming-language/python/Python-Dev-Env/",
+        "teaser": null
+      }]
