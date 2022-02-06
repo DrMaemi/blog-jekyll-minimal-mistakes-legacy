6c10239b40112bcf806988a8bc42e0ac97944222
DrMaemi <leesh1510@ajou.ac.kr>
2022-02-07 04:26:01 +0900

    How to use markdown and apply config

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -10,4 +10,16 @@ var store = [{
         "tags": ["Anaconda","Jupyter Notebook","VSCode"],
         "url": "/programming-language/python/Python-Dev-Env/",
         "teaser": null
+      },{
+        "title": "1. 파이썬 개발 환경 설정",
+        "excerpt":"파이썬 환경 설정 목차 1. 아나콘다 1.1. 설치 1.2. 가상환경 생성 1.3. 패키지 설치 1.4. 설치된 패키지 삭제 1.5. 가상환경 복제 2. 주피터 노트북 - 가상 환경 연동 3. VS Code - 가상환경 연동 1. 아나콘다 1.1. 설치 https://www.anaconda.com/products/individual - 공식 배포 버전 https://repo.anaconda.com/archive/ - 버전 별 다운, 쉘 명령어...","categories": ["Github.io","Python"],
+        "tags": ["Anaconda","Jupyter Notebook","VSCode"],
+        "url": "/github.io/python/align-image/",
+        "teaser": null
+      },{
+        "title": "Markdown 사용법",
+        "excerpt":"Markdown 사용법 목차 1. 마크다운이란? 2. 기본 문법 3. 같은 문서 내 헤더로 링크 걸기 4. 중첩된 목록 1. 마크다운이란? 마크다운은 평문(plain-text) 편집기를 사용하는 정형화된(formatted) 텍스트를 위한 경량 마크업 언어다. 마크다운은 블로그나 즉석 메시지(instant messaging), 온라인 포럼, 협업 소프트웨어 그리고 리드미(REAMDE) 파일 등에 널리 사용된다. 2. 기본 문법 마크다운의 기본...","categories": ["Programming-Language","Markdown"],
+        "tags": ["Programming-Language"],
+        "url": "/programming-language/markdown/how-to-use-markdown/",
+        "teaser": null
       }]
