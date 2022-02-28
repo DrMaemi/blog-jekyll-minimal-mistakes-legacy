763bfc25e7b04e63d5fd62d775fae4e69a396a23
DrMaemi <leesh1510@ajou.ac.kr>
2022-03-01 06:36:09 +0900

    Add js: make codeblock header & aglify to customs.min.js

---
+++ assets/js/customs/codeblock.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const code = document.getElementsByTagName('code');
+
+  Array.from(code).forEach(el => {
+    if (el.className) {
+      const s = el.className.split(':');
+      const highlightLang = s[0];
+      const filename = s[1];
+      if (filename) {
+        el.classList.remove(el.className);
+        el.classList.add(highlightLang);
+        el.parentElement.setAttribute('data-lang', filename);
+        el.parentElement.classList.add('code-block-header');
+      }
+    }
+  });
+});
