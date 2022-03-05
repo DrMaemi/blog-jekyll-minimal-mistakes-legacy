03adbca78bcd77da63b9240568a5686f6a48e219
DrMaemi <leesh1510@ajou.ac.kr>
2022-03-05 20:26:47 +0900

    Add js: highlight-line-numbers.js & Renamed js file: highlight-with-header → highlight-parser & Uglified those js into customs.min.js

--- assets/js/customs/highlight-parser.js
+++ assets/js/customs/highlight-parser.js
@@ -6,11 +6,19 @@ document.addEventListener('DOMContentLoaded', () => {
       const s = el.className.split(':');
       const highlightLang = s[0];
       const filename = s[1];
+      const lineons = s[2];
+      
       if (filename) {
         el.classList.remove(el.className);
         el.classList.add(highlightLang);
         el.parentElement.setAttribute('file-name', filename);
         el.parentElement.classList.add('with-header');
+      }
+
+      if (lineons) {
+        el.classList.remove(el.className);
+        el.classList.add(highlightLang);
+        el.classList.add(lineons);
       }
     }
   });
